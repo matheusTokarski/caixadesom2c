3526ed03d6226743380505e18bfe0de996dc1b9a
matheusTokarski <133028014+matheusTokarski@users.noreply.github.com>
2023-10-24 09:28:46 -0300

    ok

--- main.js
+++ main.js
@@ -8,5 +8,19 @@ function tocaSomClap(){
 }
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
-const listaDeTeclas = document.querySelectorAll('.tecla');
-listaDeTeclas[2]. onclick = TocaSomClap;
+function tocasomtim(){
+    document. querySelector ('#som_tecla_tim'). play ()
+}
+document. queryselector('. tecla Tim'). onclick = tocasomTim;
+
+    function tocaSomPuff(){
+         document. querySelector ('#som_tecla_puff').play()
+    }
+    document. querySelector('.tecla_puff').onclick = tocaSomPuff
+    const listaDeTeclas= document .querySelectorAll('.tecla');
+    listaDeTeclas[1].onclick = tocaSomclap;
+    let contador = 0;
+    while (contator < 9){
+    listaDeTeclas [contador].onclick = tocaSomclap;
+    }
+
